c4ba5e0f8c709265a51b5a345b5a62d275e4041d
manish745-82 <manish745@hotmail.com>
2025-09-21 20:43:27 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -6,10 +6,6 @@ variable "az_rg_west" {
   type    = string
   default = "tester-rg-west"
 }
-variable "az_rg_central" {
-  type    = string
-  default = "tester-rg-central"
-}
 variable "az_rg_location-east" {
   type    = string
   default = "east us"
@@ -17,10 +13,7 @@ variable "az_rg_location-east" {
 variable "az_rg_location-west" {
   type    = string
   default = "west us"
-}
-variable "az_rg_location-central" {
-  type    = string
-  default = "europe west"
+
 }
 variable "azurerm_virtual_network" {
   type    = string
@@ -30,8 +23,5 @@ variable "azurerm_virtual_network1" {
   type    = string
   default = "vnet-west"
 }
-variable "azurerm_virtual_network2" {
-  type    = string
-  default = "vnet-central"
-}
+
 
