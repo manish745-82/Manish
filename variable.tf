cc92c3e36b54502255379be3562b011e7de7a985
manish745-82 <manish745@hotmail.com>
2025-09-21 20:33:11 +0530

    Update variable.tf
    Resource Name, Resource Location, Vnet - Central created

--- variable.tf
+++ variable.tf
@@ -6,6 +6,10 @@ variable "az_rg_west" {
   type    = string
   default = "tester-rg-west"
 }
+variable "az_rg_central" {
+  type    = string
+  default = "tester-rg-central"
+}
 variable "az_rg_location-east" {
   type    = string
   default = "east us"
@@ -14,14 +18,20 @@ variable "az_rg_location-west" {
   type    = string
   default = "west us"
 }
+variable "az_rg_location-central" {
+  type    = string
+  default = "europe west"
+}
 variable "azurerm_virtual_network" {
   type    = string
   default = "vnet-east"
-
 }
-
 variable "azurerm_virtual_network1" {
   type    = string
   default = "vnet-west"
-
 }
+variable "azurerm_virtual_network2" {
+  type    = string
+  default = "vnet-central"
+}
+
